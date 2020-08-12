5873db4d0e0ee872989797f17c688285480f3949
dillen <asuperproject@gmail.com>
2020-08-12 14:20:02 -0400

    changed localhost in route fetch

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -93,7 +93,7 @@ inputForm.addEventListener('submit',(e)=>{
         "dateVisited": visitDate.value
     }
 
-    fetch('http://localhost:3000/entries', {
+    fetch('/entries/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json',},
         body: JSON.stringify(post_request_object),
